ab2e9da5669b8895be9aed5b13e5f25314756204
mateus040 <mateusgabrielmoreno264@gmail.com>
2023-06-23 17:12:22 -0300

    feat: interface principal

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import { db, auth } from './firebase.js';
+import { useEffect, useState } from 'react';
+import Header from './Header';
+import Post from './Post';
 
 function App() {
+
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+
+
+
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='header'>
+        <div className='center'>
+          <div className='header__logo'>
+            <a href=''><img src={process.env.PUBLIC_URL + '/images/logo.png'}></img></a>
+          </div>
+
+          {
+            (user) ?
+              <div>Olá</div>
+              :
+              <div className='header__loginForm'>
+                <form>
+                  <input type='text' placeholder='E-mail:' />
+                  <input type='password' placeholder='Senha:' />
+                  <input type='submit' name='acao' value='Entrar' />
+                </form>
+              </div>
+          }
+
+        </div>
+      </div>
     </div>
   );
 }
