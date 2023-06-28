d35f8e517a6531fc8d502bc600bf68a8852b8a45
mateus040 <mateusgabrielmoreno264@gmail.com>
2023-06-28 16:04:05 -0300

    feat: adicionando regras ao firebase e finalizando interface de posts

--- src/Post.js
+++ src/Post.js
@@ -1,12 +1,13 @@
 import { db } from './firebase.js';
 import { useEffect, useState } from 'react';
+import firebase from 'firebase/compat/app';
 
 function Post(props){
 
     const [comentarios, setComentarios] = useState([]);
 
     useEffect(() => {
-        db.collection('posts').doc(props.id).collection('comentarios').onSnapshot(function (snapshot) {
+        db.collection('posts').doc(props.id).collection('comentarios').orderBy('timestamp','desc').onSnapshot(function (snapshot) {
             setComentarios(snapshot.docs.map((document) => {
                 return { id: document.id, info: document.data() }
             }))
@@ -20,7 +21,8 @@ function Post(props){
 
         db.collection('posts').doc(id).collection('comentarios').add({
             nome: props.user,
-            comentario: comentarioAtual
+            comentario: comentarioAtual,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp()
         })
 
         alert('Comentário feito com sucesso!');
@@ -37,6 +39,7 @@ function Post(props){
 
             <div className='coments'>
 
+                <h2>Últimos Comentários:</h2>
                 {
                     comentarios.map(function (val) {
                         return (
